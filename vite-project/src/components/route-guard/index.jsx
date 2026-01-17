fb8d11407ed8efa7ea3e6a1346e3516259d9e213
pankaj4636 <8081PANKAJPASWAN@GMAIL.COM>
2026-01-17 16:57:31 +0530

    made changes in home page

--- vite-project/src/components/route-guard/index.jsx
+++ vite-project/src/components/route-guard/index.jsx
@@ -3,9 +3,9 @@ import { Navigate, useLocation } from "react-router-dom";
 
 function RouteGuard({authenticated, user,element}) {
   const location = useLocation();
-    // if (!authenticated && !location.pathname.includes("/auth")) {
-    //     return <Navigate to="/auth"/>;
-    // }
+    if (!authenticated && !location.pathname.includes("/auth")) {
+        return <Navigate to="/auth"/>;
+    }
    if (authenticated && user?.role !== "admin" && (location.pathname.includes("/admin") || location.pathname.includes("/auth"))) {
         return <Navigate to="/"/>;
     }
